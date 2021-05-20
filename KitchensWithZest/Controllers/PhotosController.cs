4d2a86d7d1025f4c87a737b1c915b29cef48ffae
Shangda <2193094@pcsupport.ac.nz>
2021-05-20 12:47:58 +1200

    update

---
+++ KitchensWithZest/Controllers/PhotosController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KitchensWithZest.Models;
+
+namespace KitchensWithZest.Controllers
+{
+    public class PhotosController : Controller
+    {
+        private KitchensWithZestEntities db = new KitchensWithZestEntities();
+
+        // GET: Photos
+        public ActionResult Index()
+        {
+            var photos = db.Photos.Include(p => p.Gallery).Include(p => p.Product);
+            return View(photos.ToList());
+        }
+
+        // GET: Photos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // GET: Photos/Create
+        public ActionResult Create()
+        {
+            ViewBag.GalleryId = new SelectList(db.Galleries, "GalleryId", "Title");
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Title");
+            return View();
+        }
+
+        // POST: Photos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(/*[Bind(Include = "PhotoId,ProductId,GalleryId,PhotoPath")]*/ Photo photo, IEnumerable<HttpPostedFileBase> photos)
+        {
+            if (ModelState.IsValid)
+            {
+                //if (photos != null)
+                //{
+                    var photoList = new List<Photo>();
+                    foreach (var file in photo.PhotoFiles)
+                    {
+                        if (file.ContentLength > 0)
+                        {
+                            string filename = Path.GetFileNameWithoutExtension(file.FileName)
+                                + DateTime.Now.ToString("yymmssfff")
+                                + Path.GetExtension(file.FileName);
+                            photo.PhotoPath = "~/Images/Photos/" + filename;
+                            filename = Path.Combine(Server.MapPath("~/Images/Photos/"), filename);
+                            file.SaveAs(filename);
+                            var pho = new Photo();
+                            photoList.Add(pho);
+                        }
+                    }
+
+                //}
+                db.Photos.Add(photo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GalleryId = new SelectList(db.Galleries, "GalleryId", "Title", photo.GalleryId);
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Title", photo.ProductId);
+            return View(photo);
+        }
+
+        // GET: Photos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GalleryId = new SelectList(db.Galleries, "GalleryId", "Title", photo.GalleryId);
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Title", photo.ProductId);
+            return View(photo);
+        }
+
+        // POST: Photos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PhotoId,ProductId,GalleryId,PhotoPath")] Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(photo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GalleryId = new SelectList(db.Galleries, "GalleryId", "Title", photo.GalleryId);
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Title", photo.ProductId);
+            return View(photo);
+        }
+
+        // GET: Photos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // POST: Photos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Photo photo = db.Photos.Find(id);
+            db.Photos.Remove(photo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
